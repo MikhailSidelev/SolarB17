6dedc3cc10aed2421fc2dba37638cdf5314dfca9
Khaliam <mikhailsidelev@gmail.com>
2023-11-02 10:28:59 -0400

    this commit reflectsthe basic structure of solar system

---
+++ src/test/java/SolarSys/main.java
@@ -0,0 +1,13 @@
+package SolarSys;
+
+public class main {
+    public static void main(String[] args) {
+        solarSysBscs solarsys = new solarSysBscs();
+        solarsys.Sun="1";
+        solarsys.Moon="2";
+        solarsys.Planets="2";
+        solarsys.Stars="10";
+        solarsys.Asteroids="17";
+
+    }
+}
---
+++ src/test/java/SolarSys/solarSysBscs.java
@@ -0,0 +1,10 @@
+package SolarSys;
+
+public class solarSysBscs {
+    String Sun;
+    String Moon;
+    String Stars;
+    String Planets;
+    String Asteroids;
+    
+}
